29a67bf46745642e2ec303f541458089d4482769
jackdpeterson <jack.peterson@gmail.com>
2024-08-02 10:27:39 -0700

    rsyslog package

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,11 @@
 class rsyslog_to_vendor::install {
-  package { 'rsyslogd':
+  package { 'rsyslog':
     ensure => installed
   }
 
   package { 'rsyslog-gnutls':
     ensure  => 'installed',
-    require => Package['rsyslogd'],
+    require => Package['rsyslog'],
     notify  => Exec['restart_rsyslogd'],
   }
 }
